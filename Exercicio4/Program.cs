c1e1ff65a63b9e3976dcf05320e6cfd5235a4892
Rodrigo Porto Schwedersky <rodrigo_schwedersky@estudante.sesisenai.org.br>
2022-02-16 16:33:57 -0300

    ex4

---
+++ Exercicio4/Program.cs
@@ -0,0 +1,15 @@
+﻿int cont = 0;
+int ultimoValor = 0;
+
+while (cont < 7)
+{
+    Console.WriteLine("valor");
+    var valor = Console.ReadLine();
+    var vInt = Convert.ToInt32(valor);
+    if (vInt < ultimoValor)
+    {
+        ultimoValor = vInt;
+    }
+    cont++;
+}
+Console.WriteLine("O menor valor é: " + ultimoValor);
