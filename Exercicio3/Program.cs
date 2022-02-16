54d8f16637c93d887543860dc929bf8eb4dd6ff6
Rodrigo Porto Schwedersky <rodrigo_schwedersky@estudante.sesisenai.org.br>
2022-02-16 16:30:18 -0300

    ex3

---
+++ Exercicio3/Program.cs
@@ -0,0 +1,15 @@
+﻿int cont = 0;
+int ultimoValor = 0;
+
+while (cont < 5)
+{
+    Console.WriteLine("valor");
+    var valor = Console.ReadLine();
+    var vInt = Convert.ToInt32(valor);
+    if (vInt > ultimoValor)
+    {
+        ultimoValor = vInt;
+    }
+    cont++;
+} 
+Console.WriteLine("O maior valor é: "+ultimoValor);
