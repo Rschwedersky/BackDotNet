5cad15dab1258c76db304696fd2ea547b11d99ea
Rodrigo Porto Schwedersky <rodrigo_schwedersky@estudante.sesisenai.org.br>
2022-02-16 16:10:37 -0300

    new

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Digite o Raio: ");
+double Radious = Convert.ToDouble(Console.ReadLine());
+double Area = Math.PI * Radious * Radious;
+Console.WriteLine("Area do circulo: " + Area);
+Console.ReadKey();
