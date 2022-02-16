11f9257d19d431567e9011cde16d726dd59402d7
Rodrigo Porto Schwedersky <rodrigo_schwedersky@estudante.sesisenai.org.br>
2022-02-16 16:23:31 -0300

    ex2

--- Exercicio2/Program.cs
+++ Exercicio2/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Digite a temperatura em Celsius: ");
+double C = Convert.ToDouble(Console.ReadLine());
+double Fahrenheit = (C * 9) / 5 + 32;
+Console.WriteLine("Temperatura em Fahrenheit: " + Fahrenheit);
+Console.ReadKey();
