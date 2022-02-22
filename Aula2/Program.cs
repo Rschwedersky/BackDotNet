02c534935d03724698d9682bb11e156400d8c861
Rodrigo Porto Schwedersky <rodrigo_schwedersky@estudante.sesisenai.org.br>
2022-02-22 12:27:21 -0300

    ex5

---
+++ Aula2/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+//var listaNomes = new List<string>();
+//listaNomes = new List<string>
+//{
+//    "rodrigo",
+//    "bruna"
+//};
+
+//listaNomes.Add("eduardo");
+
+//for (int i = 0; i < listaNomes.Count; i++)
+//{
+//    if(listaNomes[i].Length == 5)
+//       Console.WriteLine(listaNomes[i]);
+//}
+//Console.ReadKey();
+
+//var nomeComLetras = listaNomes.Where(x => x.Length == 5).ToList();
+//Console.WriteLine("{0}", String.Join(",", nomeComLetras));
+
+//String[] meuArray = new String[] { "doda", "ajdjewj" };
+int[] array = { 2, 4, 6, 8, 2, 2, 4, 5, 6, 3 };
+Array.Reverse(array);
+
+Console.WriteLine(String.Join(',', array));
+
+
+var listanumeros = new List<int>
+{ 1,2,3,4,5,6,87,3,6,8};
+
+listanumeros.Reverse();
+Console.WriteLine(String.Join(',', listanumeros));
+
+
+var listanotas1 = new List<int>
+{ 1,2,3,4};
+
+var listanotas2 = new List<int>
+{ 1,2,3,4};
+
+var listanotas3= new List<int>
+{ 1,2,3,4};
+
+var media1 = listanotas3.Sum()/ listanotas3.Count();
---
+++ exercicio5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+int cont = 0;
+List<int> lista = new List<int>();
+
+while (cont < 10)
+{
+    Console.WriteLine("valor");
+    var valor = Console.ReadLine();
+    var vInt = Convert.ToInt32(valor);
+    lista.Add(vInt);
+    cont++;
+}
+Console.WriteLine(String.Join(',', lista));
+List<int> newlista = lista.OrderByDescending(x => x).ToList();
+Console.WriteLine(String.Join(',', newlista));
