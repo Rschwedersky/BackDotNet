c571b59f6ba7f3f98ee43e94fdb374de5bb6f0e4
Rodrigo Porto Schwedersky <rodrigo_schwedersky@estudante.sesisenai.org.br>
2022-02-23 09:03:43 -0300

    exercicio8

---
+++ Exercicio8/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+namespace Ex8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Users\camba\source\repos\Rschwedersky\BackDotNet\Exercicio8\notas.json.txt";
+            var alunos = new List<Aluno>();
+            using (StreamReader sr = File.OpenText(path))
+            {
+                var stringAlunos = sr.ReadToEnd();
+                alunos = JsonSerializer.Deserialize<List<Aluno>>(stringAlunos);
+            }
+            if (alunos.Any(aluno => aluno.Nota > 5))
+            {
+                Console.WriteLine("Alunos com nota acima de 5:");
+                foreach (var aluno in alunos)
+                {
+                    if (aluno.Nota > 5)
+                        Console.WriteLine(aluno.Nome);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nenhum dos alunos teve nota acima de 5!");
+            }
+        }
+
+        public class Aluno
+        {
+            public string Nome { get; set; }
+            public float Nota { get; set; }
+        }
+    }
+}
