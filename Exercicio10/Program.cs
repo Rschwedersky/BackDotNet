c271203087f4e7f90bc5e65e9c13e17d511876f4
Rodrigo Porto Schwedersky <rodrigo_schwedersky@estudante.sesisenai.org.br>
2022-02-23 12:35:02 -0300

    ex10

---
+++ Exercicio10/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Digite o salário do funcionário: ");
+var salario = Console.ReadLine();
+var salarioDouble = double.Parse(salario);
+var percentualAumento = 0.00;
+double valorAumento = 0;
+double salarioNovo = 0;
+
+switch (salarioDouble)
+{
+    case <= 280:
+        percentualAumento = 0.20;
+        valorAumento = salarioDouble * percentualAumento;
+        salarioNovo = salarioDouble + valorAumento;
+        break;
+    case > 280 and < 700:
+        percentualAumento = 0.15;
+        valorAumento = salarioDouble * percentualAumento;
+        salarioNovo = salarioDouble + valorAumento;
+        break;
+    case > 700 and < 1500:
+        percentualAumento = 0.10;
+        valorAumento = salarioDouble * percentualAumento;
+        salarioNovo = salarioDouble + valorAumento;
+        break;
+    case > 1500:
+        percentualAumento = 0.05;
+        valorAumento = salarioDouble * percentualAumento;
+        salarioNovo = salarioDouble + valorAumento;
+        break;
+    default:
+        break;
+}
+
+Console.Write($"Salário antes do reajuste: R$ {salarioDouble}\nPercentual de aumnento aplicado: {percentualAumento*100}%\nValor do aumento: R$ {valorAumento}\nNovo salário com aumento aplicado: R$ {salarioNovo}");
+        
