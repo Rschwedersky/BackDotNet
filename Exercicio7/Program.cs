e7bf411215c68c65736295c486cf38c6fd5991d7
Rodrigo Porto Schwedersky <rodrigo_schwedersky@estudante.sesisenai.org.br>
2022-02-23 08:53:40 -0300

    Exercicio7

---
+++ Exercicio7/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Digite um numero para reverter a ordem: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+int reverser = int.Parse(numero.ToString().Reverse().Aggregate("", (s, c) => s + c));
+Console.WriteLine(reverser);
